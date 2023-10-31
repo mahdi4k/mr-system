fa23037f2639d430dbdbed27dfbe95aba72f4b30
mahdi falahati <falahati.mahdi@yahoo.com>
2023-10-31 15:10:55 +0330

    refactor pieces page & add skeleton loading

--- app/(pieces)/pieces/[...slug]/page.tsx
+++ app/(pieces)/pieces/[...slug]/page.tsx
@@ -1,7 +1,8 @@
 "use client"
-import React from 'react';
-import {Container, Flex, Group} from "@mantine/core";
-import useFirstPiecePc from "../../../utils/customHook/useFirstPiecePc";
+import React, {useEffect, useState} from 'react';
+import {Container, Flex, Group, Skeleton, Space} from "@mantine/core";
+import usePiecePc from "../../../utils/customHook/usePiecePc";
+import useLoading from "../../../utils/customHook/useLoading";
 
 type SlugType = 'motherboard' | 'cpu' | 'graphic'
 
@@ -14,16 +15,28 @@ interface Props {
 
 const Page: React.FC<Props> = ({params}) => {
 
-    const firstPiece = useFirstPiecePc(params.slug[0])
-    const secondPiece = useFirstPiecePc(params.slug[1])
+    const firstPiece = usePiecePc(params.slug[0])
+    const secondPiece = usePiecePc(params.slug[1])
     console.log(firstPiece, 'firstPiece')
+    const isLoading  = useLoading()
     return (
-        <Container size={'md'}>
-            <Group mt={'100px'} justify={'space-between'} align={'center'}>
-                {firstPiece}
-                {secondPiece}
-            </Group>
-        </Container>
+            <Container size={'md'}>
+                <Group mt={'100px'} justify={'space-between'} align={'center'}>
+                    {isLoading &&
+                        (
+                            <>
+                                <Skeleton radius={'xl'} width={400} height={300} mb="xl" />
+                                <Skeleton radius={'xl'} height={300} width={400} mb="xl" />
+                            </>
+                        )
+                    }
+
+                    {firstPiece}
+                    {secondPiece}
+                </Group>
+                <Space h="xl"/>
+                <Space h="xl"/>
+            </Container>
     );
 };
 
