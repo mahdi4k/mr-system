1a73e60df26cafcf773796ef1f4c80b3aecb2556
mahdi falahati <falahati.mahdi@yahoo.com>
2023-11-05 19:58:13 +0330

    add custom hook useAuth for dispatch addToken

---
+++ app/utils/customHook/useAuth.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { ReactNode, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useDispatch } from "react-redux";
+import { addToken } from "../../redux/features/auth";
+
+interface Props {
+    children: ReactNode;
+}
+
+export const useAuth = () => {
+    const dispatch = useDispatch()
+    const { data } = useSession()
+    dispatch(addToken(data))
+    useEffect(() => {
+        if (data) {
+            dispatch(addToken(data.user))
+        }
+    }, [data, dispatch])
+
+};
+
