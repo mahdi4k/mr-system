ac01cd92f44f7490063b56b3a3133a06f03f1dfe
mft <falahati.mahdi@yahoo.com>
2024-02-12 10:35:02 +0330

    add dashboard metadata title

---
+++ app/(routes)/dashboard/(pcParts)/cpu/add/clientAddCpu.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { Stepper } from '@mantine/core';
+import React, { useState } from 'react'
+import Step1Form from './step1-form';
+import Step2Image from './step2-image';
+
+export type ActiveStepDTO = 'step-1' | 'step-2'
+
+const ClientAddCpu = () => {
+    const [activeStep, setActiveStep] = useState<ActiveStepDTO>('step-1')
+    const [active, setActive] = useState(0);
+
+    return (
+        <Stepper active={active} onStepClick={setActive}>
+            <Stepper.Step allowStepSelect={activeStep === 'step-1'} label="مرحله اول" description="ثبت فرم">
+                <Step1Form setActive={setActive} setActiveStep={setActiveStep} />
+            </Stepper.Step>
+            <Stepper.Step label="مرحله دو" allowStepSelect={activeStep === 'step-2'} description="افزودن تصویر">
+                <Step2Image setActive={setActive} setActiveStep={setActiveStep} />
+            </Stepper.Step>
+        </Stepper>
+    )
+}
+
+export default ClientAddCpu
--- app/(routes)/dashboard/(pcParts)/cpu/add/page.tsx
+++ app/(routes)/dashboard/(pcParts)/cpu/add/page.tsx
@@ -1,25 +1,18 @@
-"use client"
-import React, {useState} from 'react'
-import {Stepper} from '@mantine/core';
-import Step1Form from "./step1-form";
-import Step2Image from "./step2-image";
+import React from 'react'
+import { Metadata } from 'next';
+import ClientAddCpu from './clientAddCpu';
+
+export const metadata: Metadata = {
+    title: 'افزودن CPU',
+    description: '',
+}
 
 export type ActiveStepDTO = 'step-1' | 'step-2'
 const Motherboard = () => {
-    const [activeStep, setActiveStep] = useState<ActiveStepDTO>('step-1')
-    const [active, setActive] = useState(0);
 
     return (
         <>
-            <Stepper active={active} onStepClick={setActive}>
-                <Stepper.Step allowStepSelect={activeStep === 'step-1'} label="مرحله اول" description="ثبت فرم">
-                    <Step1Form setActive={setActive} setActiveStep={setActiveStep}/>
-                </Stepper.Step>
-                <Stepper.Step label="مرحله دو" allowStepSelect={activeStep === 'step-2'} description="افزودن تصویر">
-                    <Step2Image setActive={setActive} setActiveStep={setActiveStep}/>
-                </Stepper.Step>
-            </Stepper>
-
+            <ClientAddCpu />
         </>
     )
 }
