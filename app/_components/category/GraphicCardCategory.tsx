8c52de92e81dbc184d64f2edd24392d9e54b6ade
mft <falahati.mahdi@yahoo.com>
2024-03-01 10:10:16 +0330

    add filter graphic card category

--- app/_components/category/GraphicCardCategory.tsx
+++ app/_components/category/GraphicCardCategory.tsx
@@ -1,4 +1,4 @@
-import { Flex, Popover, Button, Card, Group, Badge, Text, Divider, Stack, Grid, Avatar, Tooltip, Modal } from '@mantine/core'
+import { Flex, Popover, Button, Card, Group, Badge, Text, Divider, Stack, Grid, Avatar, Tooltip, Modal, Box } from '@mantine/core'
 import { IconChevronDown } from '@tabler/icons-react'
 import React, { useState } from 'react'
 import AmdOrIntelFilter from '../filters/AmdOrIntelFilter'
@@ -6,12 +6,16 @@ import Image from 'next/image'
 import LoadingCategorySkeleton from './LoadingCategorySkeleton'
 import { useDisclosure } from '@mantine/hooks'
 import ModalItems from './modalItems'
-import { Graphic, useGetGraphicsQuery } from '@/_redux/services/graphicApi'
+import { useGetGraphicsQuery } from '@/_redux/services/graphicApi'
 import { CPU } from '@/_redux/services/cpuApi'
 import { POWER } from '@/_redux/services/powerApi'
+import Link from 'next/link'
+import classes from './category.module.css'
+import CardPartPrice from '../shared/CardPartPrice'
+import AmdOrNvidiaFilter from '../filters/AmdOrNvidiaFilter'
 const GraphicCardCategory = () => {
   const [value, setValue] = useState<string[]>([]);
-  const { isSuccess, data = [], error, isLoading } = useGetGraphicsQuery()
+  const { isSuccess, data = [], error, isLoading } = useGetGraphicsQuery({ manufacturer: value })
   const [opened, { open, close }] = useDisclosure(false);
   const [modalData, setModalData] = useState<CPU[] | POWER[]>()
 
@@ -26,42 +30,53 @@ const GraphicCardCategory = () => {
       <Flex my={'md'} justify={'flex-end'}>
         <Popover width={200} position="bottom" withArrow shadow="md">
           <Popover.Target>
-            <Button variant='outline' styles={{ section: { marginLeft: '5px' } }} radius={'xl'} color="gray" leftSection={<IconChevronDown size={16} />} px='xl'>سازنده</Button>
+            <Button variant='outline' styles={{ section: { marginLeft: '5px' } }} radius={'xl'} color="gray" leftSection={<IconChevronDown size={16} />} px='xl'>سازنده پردازنده گرافیکی</Button>
           </Popover.Target>
           <Popover.Dropdown>
-            <AmdOrIntelFilter value={value} setValue={setValue} />
+            <AmdOrNvidiaFilter value={value} setValue={setValue} />
           </Popover.Dropdown>
         </Popover>
       </Flex>
-      <Divider color='#eee' mb={'md'} />
+      <Divider color='#d2d2d269' mb={'md'} />
 
       {isLoading ? <>
         <LoadingCategorySkeleton />
       </> : ''}
-      <Grid mb='xl' >
+      <Grid pb='md' mb='xl' >
         {data && data.map((data) => (
-          <Grid.Col key={data.id} span={{ base: 12, md: 6, lg: 3 }}>
-            <Card miw={'250px'} mih={352} key={data.id} shadow="sm" padding="lg" radius="md" withBorder>
-              <Card.Section mt={'0'} ta={'center'}>
-                {data.image && <Image alt={data.name} width={180} height={170} src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${data.image}`} />}
-              </Card.Section>
 
-              <Stack justify="center" align='center' mt="md" mb="xs">
-                <Badge color="pink">{data.brand}</Badge>
-                <Text fw={500}>{data.name}</Text>
-              </Stack>
-              <Group align='center' justify='center'>
-                <Tooltip styles={{ tooltip: { fontSize: '12px' } }} position="bottom" label='کارت گرافیک‌های مطابق'>
-                  <Avatar onClick={() => openModal(data.cpus)} style={{ cursor: 'pointer' }}><Image alt='graphic card' width={20} height={20} src={'/svg/cpu.svg'} /></Avatar>
-                </Tooltip>
-                <Tooltip styles={{ tooltip: { fontSize: '12px' } }} position="bottom" label='کارت گرافیک‌های مطابق'>
-                  <Avatar onClick={() => openModal(data.powers)} style={{ cursor: 'pointer' }}><Image alt='graphic card' width={20} height={20} src={'/svg/power.svg'} /></Avatar>
-                </Tooltip>
-              </Group>
+          <Grid.Col key={data.id} span={{ base: 12, md: 6, lg: 3 }}>
+
+            <Card className={classes.categoryCard} pos={'relative'} miw={'250px'} mih={375} key={data.id} shadow="sm" padding="lg" radius="md" withBorder>
+              <Link href={`/products/graphics/${data.id}`}>
+                <Card.Section mt={'0'} ta={'center'}>
+                  {data.image && <Image alt={data.name} width={180} height={170} src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${data.image}`} />}
+                </Card.Section>
+
+                <Stack h={50} justify="start" align='center' mt="md" mb="xs">
+                  <Text ta={'center'} fw={500}>{data.name}</Text>
+                </Stack>
+
+                <CardPartPrice price={data.price} />
+
+              </Link>
+              <Box pos={'absolute'} left={0} right={0} bottom={'2px'} component='div'>
+                <Divider color='#d2d2d269' mb={'4px'} />
+                <Group align='center' justify='center'>
+                  <Tooltip styles={{ tooltip: { fontSize: '12px' } }} position="bottom" label='cpu مطابق'>
+                    <Avatar onClick={(e) => { e.stopPropagation(); openModal(data.cpus); }} style={{ cursor: 'pointer' }}><Image alt='graphic card' width={20} height={20} src={'/svg/cpu.svg'} /></Avatar>
+                  </Tooltip>
+                  <Tooltip styles={{ tooltip: { fontSize: '12px' } }} position="bottom" label='پاورهای مطابق'>
+                    <Avatar onClick={() => openModal(data.powers)} style={{ cursor: 'pointer' }}><Image alt='graphic card' width={20} height={25} src={'/svg/power.svg'} /></Avatar>
+                  </Tooltip>
+                </Group>
+              </Box>
             </Card>
+
           </Grid.Col>
+
         ))}
-      </Grid>
+      </Grid >
 
       <Modal opened={opened} size={'1300px'} onClose={close} title=" ">
         <ModalItems items={modalData} />
