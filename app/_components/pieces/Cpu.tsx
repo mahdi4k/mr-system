a1ac86e2ceedeede26ee7e9ecfbb6d5f0cfa1a65
mft <falahati.mahdi@yahoo.com>
2024-03-06 23:11:13 +0330

    refactor cpu piece

--- app/_components/pieces/Cpu.tsx
+++ app/_components/pieces/Cpu.tsx
@@ -3,7 +3,7 @@
 
 import React, { FC, useEffect, useState } from 'react';
 import classes from "./pieces.module.css";
-import { Badge, Card, Drawer, Flex, Grid, Group, Tabs, Text, Tooltip } from "@mantine/core";
+import { Badge, Button, Card, Drawer, Flex, Grid, Group, Tabs, Text, Tooltip } from "@mantine/core";
 import Image from "next/image";
 import cardClasses from "../cardService/cardService.module.css";
 import { useDisclosure } from "@mantine/hooks";
@@ -14,30 +14,32 @@ import { RootState } from '@/_redux/store';
 import { ObjectIsEmpty } from '@/_utils/utils';
 import { IconBuildingStore, IconExclamationCircle, IconListDetails, IconX } from '@tabler/icons-react';
 import useLoading from '@/_utils/customHook/useLoading';
+import Link from 'next/link';
+import ShopsLink from './shared/ShopsLink';
 
 export type IcpuProps = {
-    setActiveCpu:  React.Dispatch<React.SetStateAction<Partial<CPU> | undefined>>
+    setActiveCpu: React.Dispatch<React.SetStateAction<Partial<CPU> | undefined>>
     activeCpu: Partial<CPU> | undefined
 }
-const Cpu : FC<IcpuProps> = ({setActiveCpu,activeCpu}) => {
+const Cpu: FC<IcpuProps> = ({ setActiveCpu, activeCpu }) => {
     const [opened, { open, close }] = useDisclosure(false);
     // const { isSuccess, data = [], error } = useGetCpusQuery(undefined, { skip: !loadingSession })
     const [cpuSelected, setSelectedCpu] = useState<CPU[]>()
     const dispatch = useDispatch()
-    const loadingEnd  = useLoading();
+    const loadingEnd = useLoading();
 
-    
-     
+
+
     const cpuListFromSelectedMotherboard = useSelector((state: RootState) => state.motherboard.relatedCpus);
     const cpuListFromSelectedGraphic = useSelector((state: RootState) => state.graphic.relatedCpus);
     const { isSuccess, data = [], error, } = useGetCpusQuery({});
     const currentCpu = useSelector((state: RootState) => state.cpu.selectedCpu);
     const selectedMotherboard = useSelector((state: RootState) => state.motherboard.selectedMotherboard);
-    useEffect(()=>{
-        if(loadingEnd){
+    useEffect(() => {
+        if (loadingEnd) {
             setActiveCpu(currentCpu)
         }
-    },[loadingEnd,currentCpu])
+    }, [loadingEnd, currentCpu])
     const selectedCpu = (id: number) => {
         const cpu = data.find(el => el.id === id)
         if (cpu) {
@@ -86,7 +88,7 @@ const Cpu : FC<IcpuProps> = ({setActiveCpu,activeCpu}) => {
                         <Image className={classes.piecesImg} width={60} height={60} src={'/svg/cpu.svg'} alt={'cpu'} />
                     </Flex>
                 ) : (
-                    <Card style={{ padding: '0 35px', marginTop: '20px', width: '195px' }} radius="md" shadow='md'>
+                    <Card style={{ padding: '0 35px', marginTop: '20px', width: '195px' }} radius="md" shadow='xs'>
                         <IconX onClick={removeSelected} size={18} style={{ position: 'absolute', right: '4px', top: '3px' }} />
                         <Card.Section style={{ textAlign: 'center' }} mt={'md'}>
                             {currentCpu.image && <Image alt={currentCpu.name ? currentCpu.name : ''} width={80} height={80} src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${currentCpu.image}`} />}
@@ -97,6 +99,14 @@ const Cpu : FC<IcpuProps> = ({setActiveCpu,activeCpu}) => {
                             </Badge>
                             <Text fz={"sm"} mt={'md'}>{currentCpu.name}</Text>
                         </Flex>
+                        {currentCpu.price ? <>
+                            <Group gap={3} justify="center" align='center' mb="xs">
+                                <Text fz={'xs'} ml={'2px'}>از</Text>
+                                <Text fz={'xs'} fw={'bold'}>{Intl.NumberFormat('fa', {}).format(Number(currentCpu.price))}</Text>
+                                <Image className={classes.tomanIcon} src={'/svg/toman.svg'} alt='kiwi part price' width={16} height={16} />
+                            </Group>
+                        </> : ''}
+
                     </Card>
                 )}
 
@@ -115,41 +125,18 @@ const Cpu : FC<IcpuProps> = ({setActiveCpu,activeCpu}) => {
                         <Tabs.Panel value="info">
                             <Grid my={'md'}>
                                 <Grid.Col className={classes.borderBottomDashed} span={6}><Text fz={'sm'}>گرافیک مجتمع</Text></Grid.Col>
-                                <Grid.Col className={classes.borderBottomDashed} span={6}><Text fw={'bold'} fz={'sm'}>{currentCpu.cpu_socket}</Text></Grid.Col>
+                                <Grid.Col className={classes.borderBottomDashed} span={6}><Text fw={'bold'} fz={'sm'}>{currentCpu.integrated_graphic}</Text></Grid.Col>
 
-                                <Grid.Col className={classes.borderBottomDashed} span={6}><Text fz={'sm'}>نسل رم قابل پشتیبانی</Text></Grid.Col>
+                                <Grid.Col className={classes.borderBottomDashed} span={6}><Text fz={'sm'}>سوکت</Text></Grid.Col>
                                 <Grid.Col className={classes.borderBottomDashed} span={6}>
-                                    <Flex align={'center'}>
-                                        <Text mt={'4px'} ml={'4px'} style={{ width: 'fit-content' }} fw={'bold'} fz={'sm'}> DDR4</Text>
-                                        <Text mt={'4px'} ml={'4px'} style={{ width: 'fit-content' }} fw={'bold'} fz={'sm'}> DDR3</Text>
-                                    </Flex>
+                                    <Text fw={'bold'} fz={'sm'}>{currentCpu.cpu_socket}</Text>
                                 </Grid.Col>
-                                <Grid.Col className={classes.borderBottomDashed} span={6}><Text fz={'sm'}>فرکانس‌های رم قابل پشتیبانی</Text></Grid.Col>
-                                <Grid.Col className={`${classes.borderBottomDashed} ${classes.dFlex}`} span={6}>{currentCpu.attributes?.map(el => (
-                                    <Tooltip
-                                        key={el}
-                                        withArrow
-                                        transitionProps={{ duration: 400 }}
-                                        label="خرید"
-                                    >
-                                        <Text className={classes.Badge} fw={'bold'} fz={'xs'}>{el}</Text>
-                                    </Tooltip>
-                                ))}
-                                    <Tooltip
-                                        multiline
-                                        w={420}
-                                        withArrow
-                                        transitionProps={{ duration: 400 }}
-                                        label="در هنگام خرید رم به حداکثر فرکانس قابل پشتیبانی دقت کنید. درصورتی که رم با فرکانس بالاتر از مقدار گفته شده خریداری کنید فرکانس به حداکثر مقدار مادربرد بازگردانده میشود و عملا هزینه اضافی کرده‌اید "
-                                    >
-                                        <IconExclamationCircle color='green' />
-                                    </Tooltip>
-                                </Grid.Col>
+
 
                             </Grid>
                         </Tabs.Panel>
                         <Tabs.Panel value="shop">
-                            Messages tab content
+                            <ShopsLink currentPiece={currentCpu} />
                         </Tabs.Panel>
                     </Tabs>
                 )}
@@ -162,17 +149,21 @@ const Cpu : FC<IcpuProps> = ({setActiveCpu,activeCpu}) => {
                             <Card onClick={() => selectedCpu(el.id)} key={el.id} className={cardClasses.hoverCard}
                                 style={{ padding: '0 45px' }} radius="md" withBorder>
                                 <Card.Section style={{ textAlign: 'center' }} mt={'md'}>
-
                                     {el.image && <Image alt={el.name} width={80} height={80} src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${el.image}`} />}
-
                                 </Card.Section>
 
-                                <Flex direction={'column'} justify="center" align={'center'} mt="sm" mb="xs">
-                                    <Badge color="pink" variant="light">
-                                        {el.manufacturer}
-                                    </Badge>
-                                    <Text fz={"sm"} mt={'md'}>{el.name}</Text>
+                                <Flex direction={'column'} justify="center" align={'center'} mt="xs" mb="xs">
+                                    <Text ta={'center'} fz={"sm"} >{el.name}</Text>
                                 </Flex>
+                                {el.price ? <>
+                                    <Group gap={3} justify="center" align='center' mb="xs">
+                                        <Text fz={'xs'} ml={'2px'}>از</Text>
+                                        <Text fz={'xs'} fw={'bold'}>{Intl.NumberFormat('fa', {}).format(Number(el.price))}</Text>
+                                        <Image className={classes.tomanIcon} src={'/svg/toman.svg'} alt='kiwi part price' width={16} height={16} />
+                                    </Group>
+                                </> : ''}
+
+
                             </Card>
                         ))
                     }
