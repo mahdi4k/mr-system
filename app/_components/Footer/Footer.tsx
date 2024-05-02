1a04245f71fd0f200cdabae446a026d8f7b797fe
mft <falahati.mahdi@yahoo.com>
2024-05-02 08:39:50 +0330

    refactor footer

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Container, Group, ActionIcon, rem } from '@mantine/core';
+import { Container, Group, ActionIcon, rem, Text } from '@mantine/core';
 import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
 import classes from './footer.module.css';
 
@@ -7,7 +7,9 @@ export function Footer() {
     return (
         <div className={`${classes.footer} main-footer`}>
             <Container size="lg" className={classes.inner}>
-                kiwi-part
+                <Text c="dimmed" size="sm">
+                    © 2024 kiwi part. All rights reserved
+                </Text>
                 <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
                     <ActionIcon size="lg" color="gray" variant="subtle">
                         <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
