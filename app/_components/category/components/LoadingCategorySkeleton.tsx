16249db9a2cc0faf70831fc01b604ffdd750864b
mft <falahati.mahdi@yahoo.com>
2024-06-01 22:38:41 +0330

    refactor loading category skeleton

--- app/_components/category/components/LoadingCategorySkeleton.tsx
+++ app/_components/category/components/LoadingCategorySkeleton.tsx
@@ -5,16 +5,16 @@ const LoadingCategorySkeleton = () => {
     return (
         <Grid >
             <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-                <Skeleton radius={'md'} height={350} mb="xl" />
+                <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-                <Skeleton radius={'md'} height={350} mb="xl" />
+                <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-                <Skeleton radius={'md'} height={350} mb="xl" />
+                <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-                <Skeleton radius={'md'} height={350} mb="xl" />
+                <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
         </Grid>
     )
