e7b8f6c3623e9adebd6160cc2a1999d0b443734a
mft <falahati.mahdi@yahoo.com>
2024-06-29 22:54:38 +0330

    add new cart parts

--- app/_components/cardParts/CardParts.tsx
+++ app/_components/cardParts/CardParts.tsx
@@ -52,6 +52,48 @@ const CardParts = () => {
                         <Text className={classes.cardTitle} mt={'xs'}>کارت گرافیک</Text>
                     </Link>
                 </Grid.Col>
+
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                    <Link style={{ textDecoration: 'none' }} href={'/category/ssd'} >
+                        <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
+                            <Flex justify={'center'} direction={'column'} align={'center'}>
+                                <Image width={110} height={90} src={'/svg/ssd.svg'} alt={'graphic'} />
+                            </Flex>
+                        </Card>
+                        <Text className={classes.cardTitle} mt={'xs'}>ssd</Text>
+                    </Link>
+                </Grid.Col>
+
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                    <Link style={{ textDecoration: 'none' }} href={'/category/ram'} >
+                        <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
+                            <Flex justify={'center'} direction={'column'} align={'center'}>
+                                <Image width={110} height={90} src={'/svg/ram.svg'} alt={'graphic'} />
+                            </Flex>
+                        </Card>
+                        <Text className={classes.cardTitle} mt={'xs'}>رم</Text>
+                    </Link>
+                </Grid.Col>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                    <Link style={{ textDecoration: 'none' }} href={'/category/case'} >
+                        <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
+                            <Flex justify={'center'} direction={'column'} align={'center'}>
+                                <Image width={110} height={90} src={'/svg/case.svg'} alt={'graphic'} />
+                            </Flex>
+                        </Card>
+                        <Text className={classes.cardTitle} mt={'xs'}>کیس</Text>
+                    </Link>
+                </Grid.Col>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                    <Link style={{ textDecoration: 'none' }} href={'/category/fan'} >
+                        <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
+                            <Flex justify={'center'} direction={'column'} align={'center'}>
+                                <Image width={150} height={120} src={'/svg/fan.svg'} alt={'graphic'} />
+                            </Flex>
+                        </Card>
+                        <Text className={classes.cardTitle} mt={'xs'}>فن</Text>
+                    </Link>
+                </Grid.Col>
             </Grid>
         </Container>
     )
