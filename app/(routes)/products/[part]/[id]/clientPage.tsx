fa542620b2863c8fd8fbec6d1f96aeb8ae22d6c3
mft <falahati.mahdi@yahoo.com>
2024-07-14 21:38:48 +0330

    add new single pages

--- app/(routes)/products/[part]/[id]/clientPage.tsx
+++ app/(routes)/products/[part]/[id]/clientPage.tsx
@@ -8,11 +8,15 @@ import { POWER } from '@/_redux/services/powerApi'
 import { Motherboard } from '@/_redux/services/motherboardApi'
 import dynamic from 'next/dynamic'
 import BreadCrumbKiwi from '@/_components/single/BreadCrumbKiwi'
+import { CASE } from '@/_redux/services/caseApi'
+import { FAN } from '@/_redux/services/fanApi'
+import { RAM } from '@/_redux/services/ramApi'
+import { SSD } from '@/_redux/services/ssdApi'
 
 
 type productType = {
-  product: Graphic | CPU | POWER | Motherboard
-  type: 'graphics' | 'powers' | 'motherboards' | 'cpus'
+  product: Graphic | CPU | POWER | Motherboard | CASE | FAN | RAM | SSD
+  type: 'graphics' | 'powers' | 'motherboards' | 'cpus' | 'cases' | 'rams' | 'fans' | 'ssds'
 }
 
 
@@ -20,6 +24,10 @@ const ComponentCPU = dynamic(() => import('@/_components/single/CpuSingle'), { s
 const ComponentMotherboard = dynamic(() => import('@/_components/single/MotherboardSingle'), { ssr: false })
 const ComponentGraphicCard = dynamic(() => import('@/_components/single/GraphicCardSingle'), { ssr: false })
 const ComponentPower = dynamic(() => import('@/_components/single/PowerSingle'), { ssr: false })
+const ComponentCase = dynamic(() => import('@/_components/single/CaseSingle'), { ssr: false })
+const ComponentFan = dynamic(() => import('@/_components/single/FanSingle'), { ssr: false })
+const ComponentRam = dynamic(() => import('@/_components/single/RamSingle'), { ssr: false })
+const ComponentSsd = dynamic(() => import('@/_components/single/SsdSingle'), { ssr: false })
 
 
 const ClientPage: FC<productType> = ({ product, type }) => {
@@ -51,6 +59,34 @@ const ClientPage: FC<productType> = ({ product, type }) => {
         <Container styles={{ root: { flex: '1 0 auto', width: '100%' } }} size={'lg'}>
           <BreadCrumbKiwi title={product.name} type='power' />
           <ComponentPower product={product as POWER} />
+        </Container>
+      );
+    case "cases":
+      return (
+        <Container styles={{ root: { flex: '1 0 auto', width: '100%' } }} size={'lg'}>
+          <BreadCrumbKiwi title={product.name} type='case' />
+          <ComponentCase product={product as CASE} />
+        </Container>
+      );
+    case "fans":
+      return (
+        <Container styles={{ root: { flex: '1 0 auto', width: '100%' } }} size={'lg'}>
+          <BreadCrumbKiwi title={product.name} type='fan' />
+          <ComponentFan product={product as FAN} />
+        </Container>
+      );
+    case "rams":
+      return (
+        <Container styles={{ root: { flex: '1 0 auto', width: '100%' } }} size={'lg'}>
+          <BreadCrumbKiwi title={product.name} type='ram' />
+          <ComponentRam product={product as RAM} />
+        </Container>
+      );
+    case "ssds":
+      return (
+        <Container styles={{ root: { flex: '1 0 auto', width: '100%' } }} size={'lg'}>
+          <BreadCrumbKiwi title={product.name} type='ssd' />
+          <ComponentSsd product={product as SSD} />
         </Container>
       );
     default:
