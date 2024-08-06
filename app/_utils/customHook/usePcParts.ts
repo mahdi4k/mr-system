087678d93ba2cb6cca49c5e3a99df517ef7fc6de
mft <falahati.mahdi@yahoo.com>
2024-08-06 15:29:21 +0330

    add use pc part custom hook

---
+++ app/_utils/customHook/usePcParts.ts
@@ -0,0 +1,16 @@
+const usePcparts = () => {
+    const pcParts = [
+        { name: 'cpu', link: '/category/cpu', title: 'cpu', svg: '/svg/cpu.svg' },
+        { name: 'ram', link: '/category/ram', title: 'رم', svg: '/svg/ram.svg' },
+        { name: 'graphic', link: '/category/graphic', title: 'کارت گرافیک', svg: '/svg/graphic.svg' },
+        { name: 'fan', link: '/category/fan', title: 'فن', svg: '/svg/fan.svg' },
+        { name: 'ssd', link: '/category/ssd', title: 'ssd', svg: '/svg/ssd.svg' },
+        { name: 'case', link: '/category/case', title: 'کیس', svg: '/svg/case.svg' },
+        { name: 'motherboard', link: '/category/motherboard', title: 'مادربرد', svg: '/svg/motherboard.svg' },
+        { name: 'power', link: '/category/power', title: 'پاور' , svg: '/svg/power.svg' },
+    ];
+
+    return pcParts;
+};
+
+export default usePcparts;
