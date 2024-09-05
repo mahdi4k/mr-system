d4a0f62f88486ad02dcda0fd971b468d9714871c
mft <falahati.mahdi@yahoo.com>
2024-09-05 08:25:32 +0330

    refactor use pc parts hook

--- app/_utils/customHook/usePcParts.ts
+++ app/_utils/customHook/usePcParts.ts
@@ -1,13 +1,13 @@
 const usePcparts = () => {
     const pcParts = [
         { name: 'cpu', link: '/category/cpu', title: 'cpu', svg: '/svg/cpu.svg' },
-        { name: 'ram', link: '/category/ram', title: 'رم', svg: '/svg/ram.svg' },
+        { name: 'motherboard', link: '/category/motherboard', title: 'مادربرد', svg: '/svg/motherboard.svg' },
         { name: 'graphic', link: '/category/graphic', title: 'کارت گرافیک', svg: '/svg/graphic.svg' },
+        { name: 'power', link: '/category/power', title: 'پاور' , svg: '/svg/power.svg' },
+        { name: 'ram', link: '/category/ram', title: 'رم', svg: '/svg/ram.svg' },
         { name: 'fan', link: '/category/fan', title: 'فن', svg: '/svg/fan.svg' },
         { name: 'ssd', link: '/category/ssd', title: 'ssd', svg: '/svg/ssd.svg' },
         { name: 'case', link: '/category/case', title: 'کیس', svg: '/svg/case.svg' },
-        { name: 'motherboard', link: '/category/motherboard', title: 'مادربرد', svg: '/svg/motherboard.svg' },
-        { name: 'power', link: '/category/power', title: 'پاور' , svg: '/svg/power.svg' },
     ];
 
     return pcParts;
