a6c82dcc3a5bb36142caadee0263ce534db40356
mft <falahati.mahdi@yahoo.com>
2024-09-05 08:29:13 +0330

    refactor card part and suggest section

--- app/_components/shared/CardPartPrice.tsx
+++ app/_components/shared/CardPartPrice.tsx
@@ -2,12 +2,12 @@ import { Group, Text } from '@mantine/core'
 import React from 'react'
 import Image from 'next/image'
 import classes from '../category/category.module.css'
-const CardPartPrice = ({ price, justify }: { price: string | undefined, justify?: string }) => {
+const CardPartPrice = ({ price, justify, isAds }: { price: string | undefined, justify?: string, isAds?: boolean }) => {
     return (
         <>
             {price ? <>
                 <Group justify={justify} gap={4} className={classes.Price} align='center' mt="md" mb="xs">
-                    <Text className={classes.PriceProductTitle} fw={'bold'} c='#25ac9e'>از</Text>
+                    <Text hidden={isAds} className={classes.PriceProductTitle} fw={'bold'} c='#25ac9e'>از</Text>
                     <Text className={classes.PriceProductTitle} fw={'bold'} c='#25ac9e'>{Intl.NumberFormat('fa', {}).format(Number(price))}</Text>
                     <Image src={'/svg/toman.svg'} alt='kiwi part price' width={22} height={22} />
                 </Group>
--- app/_components/suggestSection/SuggestSection.tsx
+++ app/_components/suggestSection/SuggestSection.tsx
@@ -1,57 +1,60 @@
-import { Box, Card, Container, Flex, SimpleGrid, Text } from '@mantine/core'
+"use client"
+
+import { Box, Card, Container, Flex, Grid, rem, SimpleGrid, Skeleton, Text } from '@mantine/core'
 import React from 'react'
 import SVG from "react-inlinesvg"
 import { IconMoodSmile } from '@tabler/icons-react';
 import Link from 'next/link';
 
+const PRIMARY_COL_HEIGHT = rem(300);
+
 const SuggestSection = () => {
+    const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - var(--mantine-spacing-md) / 2)`;
+
     return (
         <Container size={'lg'}>
-            <SimpleGrid cols={3} spacing={'xl'}>
-                <Card style={{ alignItems: 'center' }} h={400} shadow='sm'>
-                    <SVG
-                        style={{ flexGrow: '1' }}
-                        width={120}
-                        loader={<Box component='div' w={{ base: 300, sm: 507 }} h={{ base: 360, md: 478 }}></Box>}
-                        src='/svg/case.svg' />
-                    <Flex justify={'center'} align={'center'} mt={'lg'}>
-                        <Text pb={'xl'} mt={'lg'} ta={'center'}>
-                            سیستم آماده شماره ۱
-                        </Text>
 
-                    </Flex>
-                </Card>
-                <Card style={{ alignItems: 'center' }} shadow='md'>
-                    <Link href={'/choose-part'}>
-                        <SVG
-                            width={300}
-                            src='/svg/all-part.svg' />
-                        <Flex justify={'center'} align={'center'} mt={'lg'}>
-                            <Text fz={'2rem'} className={'vibes'} mt={'lg'} ta={'center'}>
-                                خودت انتخاب کن
-                            </Text>
-                            <Box mt={'lg'} mr={'2px'}>
-                                <IconMoodSmile size={35} color='var(--mantine-color-kiwi-8)' />
-                            </Box>
-                        </Flex>
+            <SimpleGrid cols={{ base: 1, sm: 3 }} spacing="md">
+                <Flex h={PRIMARY_COL_HEIGHT}>
+                    <Card w={'100%'} style={{ alignItems: 'center' }} shadow='md'>
+                        <Link href={'/choose-part'}>
+                            <SVG
+                                width={220}
+                                src='/svg/all-part.svg' />
+                            <Flex justify={'center'} align={'center'} mt={'sm'}>
+                                <Text fz={'2rem'} className={'vibes'} ta={'center'}>
+                                    خودت انتخاب کن
+                                </Text>
+                                <Box mt={'lg'} mr={'2px'}>
+                                    <IconMoodSmile size={35} color='var(--mantine-color-kiwi-8)' />
+                                </Box>
+                            </Flex>
 
-                    </Link>
+                        </Link>
 
-                </Card>
-                <Card style={{ alignItems: 'center' }} h={400} shadow='sm'>
-                    <SVG
-                        style={{ flexGrow: '1' }}
-                        width={120}
-                        loader={<Box component='div' w={{ base: 300, sm: 507 }} h={{ base: 360, md: 478 }}></Box>}
-                        src='/svg/case.svg' />
-                    <Flex justify={'center'} align={'center'} mt={'lg'}>
-                        <Text pb={'xl'} mt={'lg'} ta={'center'}>
-                            سیستم آماده شماره ۲
-                        </Text>
-                    </Flex>
-                </Card>
+                    </Card>
+                </Flex>
+                <Grid gutter="md">
 
+                    <Grid.Col span={12}>
+                        <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+                    </Grid.Col>
+                    <Grid.Col span={12}>
+                        <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+                    </Grid.Col>
+                </Grid>
+                <Grid gutter="md">
+
+                    <Grid.Col span={12}>
+                        <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+                    </Grid.Col>
+                    <Grid.Col span={12}>
+                        <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+                    </Grid.Col>
+                </Grid>
             </SimpleGrid>
+
+
         </Container>
     )
 }
