156fd475d9fbc5970e39a46dbfcc32fe10cdcd06
mft <falahati.mahdi@yahoo.com>
2024-09-13 16:04:13 +0330

    add ads related & gallery modal

---
+++ app/_components/adsSection/AdsGalleryModal.tsx
@@ -0,0 +1,84 @@
+import { Group, Modal } from '@mantine/core'
+import React, { FC, useEffect, useRef, useState } from 'react'
+import Image from 'next/image';
+import { Carousel } from '@mantine/carousel';
+import { EmblaCarouselType } from 'embla-carousel-react';
+
+type props = {
+    image?: string,
+    openedImageModal: boolean,
+    setOpenedImageModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const AdsGalleryModal: FC<props> = ({ image, openedImageModal, setOpenedImageModal }) => {
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+    const emblaRef = useRef<EmblaCarouselType | null>(null); // To access embla API
+    const [images, setImages] = useState<string[]>([])
+
+    useEffect(() => {
+        if (image) {
+            const images: string[] = JSON.parse(image);
+            setImages(images)
+        }
+    }, [image])
+
+
+    const handleThumbnailClick = (index: number) => {
+        setSelectedImageIndex(index);
+        emblaRef.current?.scrollTo(index); // Move carousel to clicked thumbnail
+    };
+
+    return (
+        <div>
+            <Modal
+                transitionProps={{ transition: 'pop-top-left' }}
+                opened={openedImageModal}
+                onClose={() => setOpenedImageModal(false)}
+                centered
+                size="600px" // Adjust the size automatically based on content
+            >
+                <Carousel
+                    slideSize="100%"
+                    withIndicators
+                    height={500}
+                    align="center"
+                    slideGap="md"
+                    loop
+                    draggable
+                    getEmblaApi={(embla) => (emblaRef.current = embla)}
+                    initialSlide={selectedImageIndex}
+                >
+                    {images.map((img, index) => (
+                        <Carousel.Slide key={index}>
+                            <Image
+                                width={600}
+                                height={600}
+                                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/${img}`}
+                                alt={`Large Image ${index}`} />
+                        </Carousel.Slide>
+                    ))}
+                </Carousel>
+
+
+                <Group mt="lg">
+                    {images.map((img, index) => (
+                        <Image
+                            key={index}
+                            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/${img}`}
+                            alt={`Thumbnail ${index}`}
+                            width={80}
+                            height={80}
+                            onClick={() => handleThumbnailClick(index)}
+                            style={{
+                                cursor: 'pointer',
+                                border: selectedImageIndex === index ? '2px solid blue' : 'none',
+                            }}
+                        />
+                    ))}
+                </Group>
+            </Modal>
+        </div>
+    )
+}
+
+export default AdsGalleryModal
---
+++ app/_components/adsSection/AdsRelated.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { formatJalaliTimeAgo, provinceTitleHandler, cityTitleHandler } from '@/_utils/utils';
+import { Carousel, Embla } from '@mantine/carousel'
+import { Box, Card, Flex, Group, Skeleton, Text } from '@mantine/core'
+import { IconFlag3 } from '@tabler/icons-react';
+import Link from 'next/link';
+import React, { FC, useState } from 'react'
+import CardPartPrice from '../shared/CardPartPrice';
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/_redux/store';
+import { Product } from '@/_redux/services/adsApi';
+
+type props = {
+    filteredAds: Product[],
+    isFetchingAds: boolean
+}
+
+const AdsRelated: FC<props> = ({ filteredAds, isFetchingAds }) => {
+    const [embla, setEmbla] = useState<Embla | null>(null);
+    const { ostan, city, status } = useSelector((state: RootState) => state.ads);
+
+    const handleImageCarouselAds = (image: string | undefined, title: string) => {
+        if (image) {
+            const images: string[] = JSON.parse(image);
+            return (
+                <Image alt={title} style={{ borderRadius: '7px' }} width={190} height={180}
+                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/${images[0]}`}
+                />
+            );
+
+        } else {
+            return (
+                <></>
+            )
+        }
+    }
+
+    return (
+        <Box mb={'xl'} mt={'60px'} style={{ direction: 'rtl' }}>
+            <Text>آگهی‌ های مشابه</Text>
+            <Carousel
+                slideSize={{ base: '52%', lg: '21%' }}
+                slideGap={{ base: 'sm', sm: 'md' }}
+                getEmblaApi={setEmbla}
+                align="start"
+                dragFree
+                withControls={false}>
+                {filteredAds.map(item => (
+                    <Carousel.Slide key={item.id} mt={'sm'} >
+                        <Link href={`/ads/${item.id}`}>
+                            <Card withBorder shadow="sm">
+                                <Card.Section mt={'0'} ta={'center'}>
+                                    {handleImageCarouselAds(item.image, item.title)}
+                                </Card.Section>
+                                <Text ta={'right'} mt={'5px'} lineClamp={1} fz={'md'}>{item.title}</Text>
+                                <Flex align={'baseline'} justify={'space-between'}>
+                                    <CardPartPrice isAds price={item.price} />
+                                    <Flex>
+                                        <Text ml={'2px'} fz={'xs'}>{formatJalaliTimeAgo(item.created_at)}</Text>
+                                    </Flex>
+                                </Flex>
+                                <Flex justify={'flex-end'} mt='xs' align={'baseline'}>
+                                    <IconFlag3 size={15} />
+                                    <Text style={{ position: 'relative', bottom: '3px' }} mr={'3px'} ta={'left'} fz={'11.5px'}>{provinceTitleHandler(status, item.ostan, ostan)}</Text>
+                                    <Text mr={'2px'}>,</Text>
+                                    <Text style={{ position: 'relative', bottom: '3px' }} mr={'3px'} ta={'left'} fz={'11.5px'}>{cityTitleHandler(status, item.city, city)}</Text>
+                                </Flex>
+                            </Card>
+                        </Link>
+                    </Carousel.Slide>
+                ))}
+                {isFetchingAds && (
+                    <Group mt={'md'} gap={'lg'}>
+                        <Skeleton height={'270px'} width={'220px'} />
+                        <Skeleton height={'270px'} width={'220px'} />
+                        <Skeleton height={'270px'} width={'220px'} />
+                    </Group>
+                )}
+
+            </Carousel>
+        </Box>
+    )
+}
+
+export default AdsRelated
