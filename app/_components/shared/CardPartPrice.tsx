9bc27aa238ad3a59e6a1a4e0a187854dd43f0b52
mft <falahati.mahdi@yahoo.com>
2024-09-24 12:14:26 +0330

    refactor card part price

--- app/_components/shared/CardPartPrice.tsx
+++ app/_components/shared/CardPartPrice.tsx
@@ -2,14 +2,14 @@ import { Group, Text } from '@mantine/core'
 import React from 'react'
 import Image from 'next/image'
 import classes from '../category/category.module.css'
-const CardPartPrice = ({ price, justify, isAds }: { price: string | undefined, justify?: string, isAds?: boolean }) => {
+const CardPartPrice = ({ price, justify, isAds, tomanWidth, tomanHeight, textSize }: { price: string | undefined, justify?: string, isAds?: boolean, tomanWidth?: number, tomanHeight?: number, textSize?: any }) => {
     return (
         <>
             {price ? <>
                 <Group justify={justify} gap={4} className={classes.Price} align='center' mt="md" mb="xs">
                     <Text hidden={isAds} className={classes.PriceProductTitle} fw={'bold'} c='#25ac9e'>از</Text>
-                    <Text className={classes.PriceProductTitle} fw={'bold'} c='#25ac9e'>{Intl.NumberFormat('fa', {}).format(Number(price))}</Text>
-                    <Image src={'/svg/toman.svg'} alt='kiwi part price' width={22} height={22} />
+                    <Text fz={textSize} className={classes.PriceProductTitle} fw={'bold'} c='#25ac9e'>{Intl.NumberFormat('fa', {}).format(Number(price))}</Text>
+                    <Image src={'/svg/toman.svg'} alt='kiwi part price' width={tomanWidth ? tomanWidth : 22} height={tomanHeight ? tomanHeight : 22} />
                 </Group>
             </> : ''}
         </>
