6b13955380550680ca2f77aff97fc3bd6dc99828
mft <falahati.mahdi@yahoo.com>
2024-10-15 15:03:20 +0330

    add debounce util

--- app/_utils/utils.ts
+++ app/_utils/utils.ts
@@ -150,3 +150,12 @@ export const currentUrlCategory = (item: string): string => {
     return `${window.location.pathname}?${searchParams.toString()}`;
 
 }
+
+export function debounce<T extends (...args: any[]) => void>(func: T, wait: number): (...args: Parameters<T>) => void {
+    let timeout: NodeJS.Timeout;
+    return (...args: Parameters<T>) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func(...args), wait);
+    };
+}
+
