053d12fdd08f5237680dcd71c5b78887b5f75d3b
mahdi <falahati.mahdi@yahoo.com>
2024-12-14 19:28:08 +0330

    fix issue confirm delete ads popover

--- app/(routes)/dashboard/ads/clientAds.tsx
+++ app/(routes)/dashboard/ads/clientAds.tsx
@@ -6,6 +6,7 @@ import { notifications } from '@mantine/notifications'
 import { IconCheck, IconTrash, IconXboxX } from '@tabler/icons-react'
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image';
+import ConfirmDeletePopover from '@/_components/adsSection/ConfirmDeletePopover'
 
 const ClientAds = () => {
   const [currentPage, setCurrentPage] = useState(1); // Track current page
@@ -115,43 +116,37 @@ const ClientAds = () => {
     <Table.Tr key={product.id}>
       <Table.Td>{product.id}</Table.Td>
       <Table.Td>{product.title}</Table.Td>
-      <Table.Td>
-        {handleImageAds(product.image, product.title)}
+      <Table.Td>{handleImageAds(product.image, product.title)}</Table.Td>
+      <Table.Td onClick={() => { setDescriptionTitle(product.description); open(); }}>
+        <Text fz="sm" lineClamp={1}>
+          {product.description}
+        </Text>
       </Table.Td>
-
-      <Table.Td onClick={() => { setDescriptionTitle(product.description); open() }}><Text fz='sm' lineClamp={1}>{product.description}</Text></Table.Td>
       <Table.Td>{product.category.name}</Table.Td>
       <Table.Td>{product.user.username}</Table.Td>
       <Table.Td>{new Intl.NumberFormat('fa-IR').format(Number(product.price))}</Table.Td>
-      <Table.Td>{product.status === 'approved' ? <Text fz={'xs'} c={'green'}>Approved</Text> : <Text fz={'xs'} c={'red'}>Pending</Text>}</Table.Td>
+      <Table.Td>
+        {product.status === 'approved' ? (
+          <Text fz="xs" c="green">Approved</Text>
+        ) : (
+          <Text fz="xs" c="red">Pending</Text>
+        )}
+      </Table.Td>
       <Table.Td>{product.city}</Table.Td>
       <Table.Td>
         <Flex>
-          <ActionIcon onClick={() => acceptAds(product.id)} variant='light' ml={'lg'}>
+          <ActionIcon onClick={() => acceptAds(product.id)} variant="light" ml={'lg'}>
             <IconCheck size={17} />
           </ActionIcon>
-          <ActionIcon onClick={() => rejectAds(product.id)} variant='light' color='orange'>
+          <ActionIcon onClick={() => rejectAds(product.id)} variant="light" color="orange">
             <IconXboxX size={17} />
           </ActionIcon>
-          <Popover opened={opened[product.id] || false}
-            onClose={() => handleClose(product.id)} width={200} position="bottom" withArrow shadow="md" >
-            <Popover.Target>
-              <ActionIcon onClick={() => handleOpen(product.id)} mr={'lg'} variant='light' color='red'>
-                <IconTrash size={17} />
-              </ActionIcon>
-            </Popover.Target>
-            <Popover.Dropdown>
-              <Text size="xs">آیا از حذف این آیتم مطمن هستید</Text>
-              <Flex justify={'flex-end'} mt={'sm'}>
-                <Button ml={'sm'} color={'red'} variant='light' size='xs' onClick={() => removeAds(product.id)}>بله</Button>
-                <Button variant='light' size='xs' onClick={() => handleClose(product.id)} >خیر</Button>
-              </Flex>
-            </Popover.Dropdown>
-          </Popover>
+          <ConfirmDeletePopover productId={product.id} onDelete={removeAds} />
         </Flex>
       </Table.Td>
     </Table.Tr>
   ));
+  
   const totalPages = data?.last_page || 1;
 
   return (
---
+++ app/_components/adsSection/ConfirmDeletePopover.tsx
@@ -0,0 +1,30 @@
+import { Popover, ActionIcon, Button, Text, Flex } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconTrash } from '@tabler/icons-react';
+
+const ConfirmDeletePopover = ({ productId, onDelete }: { productId: number; onDelete: (id: number) => void }) => {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <Popover opened={opened} onClose={close} width={200} position="bottom" withArrow shadow="md">
+      <Popover.Target>
+        <ActionIcon onClick={open} mr={'lg'} variant="light" color="red">
+          <IconTrash size={17} />
+        </ActionIcon>
+      </Popover.Target>
+      <Popover.Dropdown>
+        <Text size="xs">آیا از حذف این آیتم مطمن هستید</Text>
+        <Flex justify={'flex-end'} mt={'sm'}>
+          <Button ml={'sm'} color="red" variant="light" size="xs" onClick={() => onDelete(productId)}>
+            بله
+          </Button>
+          <Button variant="light" size="xs" onClick={close}>
+            خیر
+          </Button>
+        </Flex>
+      </Popover.Dropdown>
+    </Popover>
+  );
+};
+
+export default ConfirmDeletePopover
