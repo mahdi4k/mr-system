9b9b6f77a63a03af99ce121b0c884fadfda45436
mft <falahati.mahdi@yahoo.com>
2024-12-16 22:33:57 +0330

    refactor responsive card part

--- app/_components/cardParts/CardParts.tsx
+++ app/_components/cardParts/CardParts.tsx
@@ -9,7 +9,7 @@ const CardParts = () => {
     return (
         <Container mb={'xl'} size="lg">
             <Grid justify='center' py={'xl'}>
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/cpu'} >
                         <Card className={classes.card} bg={'var(--mantine-color-indigo-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
@@ -20,7 +20,7 @@ const CardParts = () => {
                     </Link>
                 </Grid.Col>
 
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/motherboard'} >
                         <Card className={classes.card} bg={'var(--mantine-color-cyan-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
@@ -31,7 +31,7 @@ const CardParts = () => {
                     </Link>
                 </Grid.Col>
 
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/power'}>
                         <Card className={classes.card} bg={'var(--mantine-color-green-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
@@ -42,7 +42,7 @@ const CardParts = () => {
                     </Link>
                 </Grid.Col>
 
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/graphic'} >
                         <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
@@ -53,7 +53,7 @@ const CardParts = () => {
                     </Link>
                 </Grid.Col>
 
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/ssd'} >
                         <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
@@ -64,7 +64,7 @@ const CardParts = () => {
                     </Link>
                 </Grid.Col>
 
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/ram'} >
                         <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
@@ -74,7 +74,7 @@ const CardParts = () => {
                         <Text className={classes.cardTitle} mt={'xs'}>رم</Text>
                     </Link>
                 </Grid.Col>
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/case'} >
                         <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
@@ -84,7 +84,7 @@ const CardParts = () => {
                         <Text className={classes.cardTitle} mt={'xs'}>کیس</Text>
                     </Link>
                 </Grid.Col>
-                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{ base: 12, sm: 6, lg: 3 }}>
+                <Grid.Col style={{ display: 'flex', justifyContent: 'center' }} span={{  base: 6, lg: 3 }}>
                     <Link style={{ textDecoration: 'none' }} href={'/category/fan'} >
                         <Card className={classes.card} bg={'var(--mantine-color-violet-outline-hover)'} mih={'160px'} radius={'md'}>
                             <Flex justify={'center'} direction={'column'} align={'center'}>
