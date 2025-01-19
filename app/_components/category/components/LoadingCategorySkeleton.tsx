64dfee53859d2bb3335b418b94df773542bdc218
mft <falahati.mahdi@yahoo.com>
2025-01-19 12:19:31 +0330

    refactor responsive loading category skeleton

--- app/_components/category/components/LoadingCategorySkeleton.tsx
+++ app/_components/category/components/LoadingCategorySkeleton.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 const LoadingCategorySkeleton = () => {
     return (
         <Grid >
-            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+            <Grid.Col span={{ base: 12, xs:6, sm: 4, lg: 3 }}>
                 <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+            <Grid.Col span={{ base: 12, xs:6, sm: 4, lg: 3 }}>
                 <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+            <Grid.Col span={{ base: 12, xs:6, sm: 4, lg: 3 }}>
                 <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+            <Grid.Col span={{ base: 12, xs:6, sm: 4, lg: 3 }}>
                 <Skeleton radius={'md'} h={{base:250,md:350}} mb={{base:'md',md:'xl'}} />
             </Grid.Col>
         </Grid>
