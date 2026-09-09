ad6c577641cb87a17f91bed8ef8c49102aa58803
mahdi <falahati.mahdi@yahoo.com>
2026-09-09 20:14:26 +0330

    feat: adjust padding in LoginPage and LoginModal for improved layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,14 +7,14 @@ export default function LoginPage() {
   return (
     <Flex h="100%" justify="center" align="center">
       <Container
-        styles={{ root: { flex: "1 0 auto", width: "100%" } }}
+        styles={{ root: { flex: "1 0 auto", width: "100%", padding: "20px" } }}
         size="xl"
       >
         <Card mx="auto" maw={400}>
           <LoginModal
             style={{
               width: "100%",
-              padding: "23px",
+              padding: "17px",
               borderRadius: "10px",
               border: "1px solid #a0bb5f",
             }}
